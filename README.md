d1bd697d65c4108f7073fc2e3a0e08c46fece192
Mitch Allen <github@mitchallen.com>
2024-01-19 10:54:27 -0500

    feat: init commit

---
+++ README.md
@@ -0,0 +1,52 @@
+express-router-gotcha
+==
+
+
+## Usage
+
+```sh
+npm start
+```
+
+```js
+// GET /alpha
+app.use(genRouter('alpha')); // will just call alpha middleware
+// GET /beta
+app.use(genRouter('beta'));  // will call alpha, beta middleware
+// routers have same parent
+// GET /v1/gamma
+app.use('/v1', genRouter('gamma')); // alpha, beta, gamma mw
+// GET /v1/delta
+app.use('/v1',genRouter('delta')); // alpha, beta, gamma, delta
+// router has unique parent 
+// GET /v2/epsilon
+app.use('/v2', genRouter('epislon')); // alpha, beta, epsilon
+```
+
+```sh
+------------------------- 
+... calling middleware: alpha 
+... calling middleware: beta 
+------------------------- 
+... calling middleware: alpha 
+... calling endpoint: alpha 
+------------------------- 
+... calling middleware: alpha 
+... calling middleware: beta 
+... calling endpoint: beta 
+------------------------- 
+... calling middleware: alpha 
+... calling middleware: beta 
+... calling middleware: gamma 
+... calling endpoint: gamma 
+------------------------- 
+... calling middleware: alpha 
+... calling middleware: beta 
+... calling middleware: gamma 
+... calling middleware: delta 
+... calling endpoint: delta 
+------------------------- 
+... calling middleware: alpha 
+... calling middleware: beta 
+... calling middleware: epislon 
+```
---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+
+const VERSION = process.env.npm_package_version;
+const API_TITLE = process.env.npm_package_name;
+
+function genRouter(name) {
+    const endpoint = `/${name}`
+    console.log(`... creating: ${endpoint}`)
+    const r = express.Router()
+    r.use(function (req, res, next) {
+        console.log(`... calling middleware: ${name} `)
+        next()
+    })
+    r.get( endpoint, (req, res) => {
+        console.log(`... calling endpoint: ${name} `)
+        const record = {
+            name,
+        }
+        res.status(200).json(record);
+    })
+    return r;
+}
+
+app.use(function (_, _, next) {
+    // log new border with each call
+    console.log(`------------------------- `)
+    next()
+})
+
+// GET /alpha
+app.use(genRouter('alpha')); // will just call alpha middleware
+// GET /beta
+app.use(genRouter('beta'));  // will call alpha, beta middleware
+// routers have same parent
+// GET /v1/gamma
+app.use('/v1', genRouter('gamma')); // alpha, beta, gamma mw
+// GET /v1/delta
+app.use('/v1',genRouter('delta')); // alpha, beta, gamma, delta
+// router has unique parent 
+// GET /v2/epsilon
+app.use('/v2', genRouter('epislon')); // alpha, beta, epsilon
+
+
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => {
+  console.log(`${API_TITLE}@${VERSION}`)
+  console.log(`running on ports INTERNAL: ${PORT}`);
+});
+
+process.on('SIGINT', () => {
+    console.log('\nSIGINT signal received: closing HTTP server')
+    server.close(() => {
+      console.log('HTTP server closed')
+    })
+    process.exit();
+  })
+  
+  process.on('SIGTERM', () => {
+    console.log('\nSIGTERM signal received: closing HTTP server')
+    server.close(() => {
+      console.log('HTTP server closed')
+    })
+    process.exit();
+  })
